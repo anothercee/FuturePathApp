1f1b0fe5cbdcfe2c9d7b4215ec59f4e8096b1b72
anothercee <anothersideofcee17@gmail.com>
2026-03-11 11:22:14 +0700

    Add majors data with associated careers

---
+++ js/majors-data.js
@@ -0,0 +1,103 @@
+const majors = [
+
+{
+name:"Informatika",
+career:["Software Engineer","AI Engineer","Data Scientist"]
+},
+
+{
+name:"Sistem Informasi",
+career:["System Analyst","IT Consultant","Product Manager"]
+},
+
+{
+name:"Teknik Industri",
+career:["Supply Chain Manager","Operations Manager","Quality Engineer"]
+},
+
+{
+name:"Kedokteran",
+career:["Dokter","Peneliti Medis","Spesialis"]
+},
+
+{
+name:"Psikologi",
+career:["Psikolog","HR Specialist","Konselor"]
+},
+
+{
+name:"Manajemen",
+career:["Business Manager","Entrepreneur","Marketing Manager"]
+},
+
+{
+name:"Akuntansi",
+career:["Akuntan","Auditor","Financial Analyst"]
+},
+
+{
+name:"Ilmu Komunikasi",
+career:["Public Relations","Content Strategist","Media Planner"]
+},
+
+{
+name:"Desain Komunikasi Visual",
+career:["Graphic Designer","Art Director","UI Designer"]
+},
+
+{
+name:"Arsitektur",
+career:["Arsitek","Urban Planner","Interior Designer"]
+},
+
+{
+name:"Farmasi",
+career:["Apoteker","Peneliti Obat","Quality Control"]
+},
+
+{
+name:"Hukum",
+career:["Pengacara","Notaris","Legal Consultant"]
+},
+
+{
+name:"Statistika",
+career:["Data Analyst","Data Scientist","Research Analyst"]
+},
+
+{
+name:"Matematika",
+career:["Data Scientist","Aktuaris","Peneliti"]
+},
+
+{
+name:"Biologi",
+career:["Peneliti","Bioteknologi","Ahli Lingkungan"]
+},
+
+{
+name:"Kimia",
+career:["Chemist","Quality Analyst","Peneliti"]
+},
+
+{
+name:"Fisika",
+career:["Peneliti","Engineer","Data Analyst"]
+},
+
+{
+name:"Hubungan Internasional",
+career:["Diplomat","Analis Politik","International Consultant"]
+},
+
+{
+name:"Ilmu Politik",
+career:["Policy Analyst","Politician","Government Consultant"]
+},
+
+{
+name:"Ekonomi",
+career:["Economist","Financial Analyst","Policy Researcher"]
+}
+
+]
